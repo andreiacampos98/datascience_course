a7f0cdd017998b2fce864bd42473fd55106cdcf2
andreiacampos98 <andreiacampos98.15@gmail.com>
2025-02-07 16:12:09 +0000

    add exercise evaluation db

--- 2_database_fundamentals/exercicio_avaliação/create_tables.sql
+++ 2_database_fundamentals/exercicio_avaliação/create_tables.sql
@@ -1,23 +1,23 @@
 CREATE TABLE espetador(
 	email character varying PRIMARY KEY,
-	nome character varying,
-	cidade character varying
+	nome character varying NOT NULL,
+	cidade character varying NOT NULL
 );
 
 CREATE TABLE artista (
 	nif character varying PRIMARY KEY,
-	nome character varying,
-	d_nasc date,
-	tipo character varying
+	nome character varying NOT NULL,
+	d_nasc date NOT NULL,
+	tipo character varying NOT NULL
 );
 
 CREATE TABLE espetaculo (
 	id character varying PRIMARY KEY,
-	titulo character varying,
-	dia date,
+	titulo character varying NOT NULL,
+	dia date NOT NULL,
 	hora int NOT NULL,
-	preco int NOT NULL,
-	categoria character varying,
+	preco int NOT NULL CHECK (preco >= 0),
+	categoria character varying NOT NULL,
 	artista character varying REFERENCES artista(nif)
 );
 
@@ -25,7 +25,7 @@ CREATE TABLE bilhete (
 	id character varying REFERENCES espetaculo(id),
 	lugar character varying,
 	email  character varying REFERENCES espetador(email),
-	custo int NOT NULL,
+	custo int NOT NULL CHECK (custo >= 0),
 	PRIMARY KEY( id, lugar)
 );
 
